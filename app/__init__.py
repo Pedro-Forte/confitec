1b861296a4249f134cf374506c03059367075a99
Pedro-Forte <pedro_forte123@hotmail.com>
2025-09-23 17:09:01 -0300

    first route complete

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import os
 
 from flask import Flask 
 from asgiref.wsgi import WsgiToAsgi
+from .views.top_ten import top_ten_bp
 
 def create_app():
 
     app = Flask(__name__)
+    app.register_blueprint(top_ten_bp)
 
     @app.get("/api/v1/health")
     def health():
---
+++ app/views/top_ten.py
@@ -0,0 +1,78 @@
+from flask import Blueprint, jsonify, request
+import requests
+import json
+
+
+top_ten_bp = Blueprint("top_ten", __name__, url_prefix="/api/v1/top_ten")
+credentials = {}
+with open('app/configs/credentials.json', 'r') as file:
+    credentials = json.load(file)
+BASE_HEADERS = {'Authorization': f'Bearer {credentials["token"]}'}
+BASE_URL = "https://api.genius.com"
+print(BASE_HEADERS)
+
+@top_ten_bp.route("", methods=["POST"])
+def get_top_ten():
+
+    data = request.get_json()
+    artist_id = data.get('id')
+
+    artist_data = get_artist_id(artist_id)
+    songs = get_songs(artist_data["name"])
+    result = process_list_songs(songs)
+
+    return jsonify(result), 200
+
+def get_artist_id(id: str):
+    """
+    receives artist id then return an object with all data
+    """
+    url = f"{BASE_URL}/artists/{id}"
+
+    response = requests.get(url, headers=BASE_HEADERS)
+
+    if response.status_code == 200:
+        return response.json()["response"]["artist"]
+    
+    else:
+        return None
+
+def get_songs(artists: str) -> list: 
+    """
+    receives artists name then returns a list of songs
+    """
+    url = f"{BASE_URL}/search"
+    params = {"q": artists}
+
+    response = requests.get(params=params, url=url, headers=BASE_HEADERS)
+
+    if response.status_code == 200:
+        return response.json()["response"]["hits"]
+    
+    else:
+        return None
+    
+def process_list_songs(songs: list) -> list:
+    """
+    receives a list of songs then return a top ten list sorted by 'pageviews'
+    """
+    hash_map = {}
+    for song in songs:
+        exist = song["result"]["stats"]["pageviews"]
+        if not hash_map.get(exist):
+            hash_map[exist] = [song]
+        else:
+            hash_map[exist].append(song)
+    
+    sorted_keys = sorted(hash_map.keys())
+    return_list = []
+
+    while len(return_list) < 10 and len(sorted_keys) > 0:
+        p_key = sorted_keys.pop()
+        for s in hash_map[p_key]:
+            return_list.append(s)
+
+    return return_list
+
+        
+
